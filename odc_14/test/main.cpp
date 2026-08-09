772bf2168b53277c34412b8622ee8170b747b594
tukanog <adasmik2017@gmail.com>
2026-08-09 17:45:11 +0200

    odcinek 14.

---
+++ odc_14/test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int tablica[2];
+
+
+int main()
+{
+    tablica[0] = 1;
+    tablica[1] = 2;
+    cout << tablica[1,96 / 3] << endl;
+    return 0;
+}
